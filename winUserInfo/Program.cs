d350f8034229f43cab9b38551fd10a8b585cd64e
Gerald Büchler <dev@genomast.de>
2021-04-27 13:30:34 +0200

    Added switch and output for "--user"

--- winUserInfo/Program.cs
+++ winUserInfo/Program.cs
@@ -10,6 +10,29 @@ namespace winUserInfo
     {
         static void Main(string[] args)
         {
+            bool showUserName = false;
+            foreach (String arg in args)
+            {
+                if (arg.ToUpper().Equals("-U") ||
+                    arg.ToUpper().Equals("--USER")
+                    )
+                {
+                    showUserName = true;
+                }
+            }
+
+            String msg = String.Empty;
+            msg += Environment.NewLine;
+
+            if (showUserName)
+            {
+                msg += "Username: " + Environment.UserName;
+            }
+
+            msg += Environment.NewLine;
+
+            Console.WriteLine(msg);
+
         }
     }
 }
