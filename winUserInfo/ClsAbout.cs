62656703b5a2e40311b4318e9ee1b05b0143b42a
Gerald Büchler <dev@genomast.de>
2021-04-27 13:42:16 +0200

    Added ClsAbout.cs to project

---
+++ winUserInfo/ClsAbout.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Reflection;
+
+namespace uptime
+{
+    class ClsAbout
+    {
+        private Assembly myAssembly;
+
+        public ClsAbout()
+        {
+            myAssembly = Assembly.GetCallingAssembly();
+        }
+
+        public String getVersion()
+        {
+            return myAssembly.GetName().Version.ToString();
+        }
+
+        public string getCopyright()
+        {
+            object[] attribs = myAssembly.GetCustomAttributes(typeof(AssemblyCopyrightAttribute), true);
+            if (attribs.Count() > 0)
+            {
+                return ((AssemblyCopyrightAttribute)attribs[0]).Copyright;
+            }
+            return "Not Found";
+        }
+
+        public string getCompany()
+        {
+            object[] attribs = myAssembly.GetCustomAttributes(typeof(AssemblyCompanyAttribute), true);
+            if (attribs.Count() > 0)
+            {
+                return ((AssemblyCompanyAttribute)attribs[0]).Company;
+            }
+            return "Not Found";
+        }
+
+        public string getProductName()
+        {
+            object[] attribs = myAssembly.GetCustomAttributes(typeof(AssemblyProductAttribute), true);
+            if (attribs.Count() > 0)
+            {
+                return ((AssemblyProductAttribute)attribs[0]).Product;
+            }
+            return "Not Found";
+        }
+    }
+}
